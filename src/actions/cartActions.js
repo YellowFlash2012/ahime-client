fe8804b561f909b481ce881d9ebe05c6b7654143
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-30 15:43:20 +0100

    configured cart reducer and addtocart action

---
+++ src/actions/cartActions.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+
+
+export const addTocart = (id, qty) => async (dispatch, getSate) => {
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty
+        }
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getSate().cart.cartItems));
+}
