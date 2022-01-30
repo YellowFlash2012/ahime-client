2b35de1bb49441cdc2c46e878bd1bfb3cb3563b0
YellowFlash2012 <cyclop9@hotmail.com>
2022-01-30 09:46:20 +0100

    implemented redux for fetching products and updated the home and single product components with dispatched actions from reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// redux imports
+
+import store from './store';
+
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+// import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,44 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { Col, Row } from "react-bootstrap";
+import { listProducts } from "../actions/productActions";
+
+import {useDispatch, useSelector} from 'react-redux'
 
 import Product from "../components/Product";
-
-import axios from 'axios';
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 
 const Home = () => {
 
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
+        dispatch(listProducts());
+    }, [dispatch])
 
-            setProducts(data);
-        }
-
-        fetchProducts();
-    }, [])
 
     return (
         <div>
             <h1>Latest products</h1>
-
-            <Row>
-                {products.map((product) => {
-                    return (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    );
-                })}
-            </Row>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <Row>
+                    {products.map((product) => {
+                        return (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            )}
         </div>
     );
 };
